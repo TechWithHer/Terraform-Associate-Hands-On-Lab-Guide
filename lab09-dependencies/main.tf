46254348f75b0b7d29abbe9e516f987ce8f949f1
Ayushi <ayushi2530@gmail.com>
2026-06-18 14:19:51 +0800

    git ignore added

--- lab09-dependencies/main.tf
+++ lab09-dependencies/main.tf
@@ -1,7 +1,44 @@
 terraform {
-	required_providers {
-
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 
 }
 
+resource "random_pet" "server" {
+  length    = 3
+  separator = "-"
+}
 
+resource "local_file" "server_name" {
+  content  = random_pet.server.id
+  filename = "${path.module}/server_name.txt"
+}
+
+resource "null_resource" "example" {
+  depends_on = [local_file.server_name]
+  triggers = {
+    server_name = random_pet.server.id
+  }
+}
+
+resource "random_id" "rotating" {
+  byte_length = 8
+  keepers = {
+    server_name = random_pet.server.id
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
